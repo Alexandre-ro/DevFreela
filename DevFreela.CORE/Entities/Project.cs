08ec519fb467ba3d50e552530e596d53c8e4ee3b
Alexandre R Oliveira <alexandre.ri.oliveira@gmail.com>
2024-01-29 22:42:33 -0300

    Refactored and created relationships in between tables.

--- DevFreela.CORE/Entities/Project.cs
+++ DevFreela.CORE/Entities/Project.cs
@@ -6,20 +6,22 @@ namespace DevFreela.Core.Entities
     {
         public Project(string title, string description, int idClient, int idFreelance, decimal totalCoast)
         {
-            Title       = title;
-            Description = description;
-            IdClient    = idClient;
-            IdFreelance = idFreelance;
-            TotalCoast  = totalCoast;
-            CreatedAt   = DateTime.Now;
-            Status      = ProjectStatusEnum.Created;
-            Comments    = new List<ProjectComment>();
+            Title           = title;
+            Description     = description;
+            IdClient        = idClient;
+            IdFreelancer    = idFreelance;
+            TotalCoast      = totalCoast;
+            CreatedAt       = DateTime.Now;
+            Status          = ProjectStatusEnum.Created;
+            Comments        = new List<ProjectComment>();
         }
 
         public string Title { get; private set; }
         public string Description { get; private set; }
+        public User Client { get; set; }
         public int IdClient { get; private set; }
-        public int IdFreelance { get; private set; }
+        public User Freelancer { get; set; }
+        public int IdFreelancer { get; private set; }
         public decimal TotalCoast { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public DateTime? StartedAt { get; private set; }
--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -10,11 +10,72 @@ namespace DevFreela.Infrastructure.Persistence
 
         }
 
+ 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Primary Key            
+            
+            modelBuilder.Entity<Project>()
+                        .ToTable("project")
+                        .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Project>()
+                        .HasOne(p => p.Freelancer)
+                        .WithMany(f => f.FreelanceProjects)
+                        .HasForeignKey(p => p.IdFreelancer)
+                        .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Project>()
+                        .HasOne(p => p.Client)
+                        .WithMany(p => p.OwnedProjects)
+                        .HasForeignKey(p => p.IdClient)
+                        .OnDelete(DeleteBehavior.Restrict);
+
+
+            modelBuilder.Entity<ProjectComment>()
+                        .ToTable("project_comment")
+                        .HasKey(p => p.Id);
+
+
+            modelBuilder.Entity<ProjectComment>()
+                        .HasOne(p => p.Project)
+                        .WithMany(p => p.Comments)
+                        .HasForeignKey(p => p.IdProject)
+                        .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<ProjectComment>()
+                        .HasOne(p => p.User)
+                        .WithMany(p => p.Comments)
+                        .HasForeignKey(p => p.IdUser)
+                        .OnDelete(DeleteBehavior.Restrict);
+
+
+            modelBuilder.Entity<Skill>()
+                        .ToTable("skill")
+                        .HasKey(s => s.Id);
+
+            modelBuilder.Entity<User>()
+                         .ToTable("user")
+                         .HasKey(u => u.Id);
+
+            modelBuilder.Entity<User>()
+                        .HasMany(u => u.Skills)
+                        .WithOne()
+                        .HasForeignKey(u => u.IdSkill)
+                        .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<UserSkill>()
+                        .ToTable("user_skill")
+                        .HasKey(u => u.Id);
+        }
+
+
+
         public DbSet<Project> Projects { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Skill> Skills { get; set; }
         public DbSet<ProjectComment> Comments { get; set; }
-        public DbSet<UserSkill> UserSkills { get; set; }    
+        public DbSet<UserSkill> UserSkills { get; set; }
 
     }
 }
