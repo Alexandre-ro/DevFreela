2d43604657816bfd3633d92144b3491a04894d12
Alexandre R Oliveira <alexandre.ri.oliveira@gmail.com>
2024-03-31 21:05:26 -0300

    Created Class for Test Projects.

---
+++ DevFreela.UnitTest/Entities/ProjectTests.cs
@@ -0,0 +1,33 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+
+namespace DevFreela.UnitTest.Entities
+{
+    public class ProjectTests
+    {
+
+        [Fact]
+        public void TestProjectsStartsWorks() 
+        {
+            var project = new Project("Teste Unitário","Desc teste",1,1,10000);
+
+            //Testa o status antes de startar o projeto
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+
+            //Testa a data antes de startar o projeto
+            Assert.NotNull(project.Description);
+            Assert.NotNull(project.Title);
+            Assert.Null(project.StartedAt);
+
+            //Starta o projeto
+            project.Start();
+
+            //Testa o status após startar
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status );
+            
+            //Testa a data após startar
+            Assert.NotNull(project.StartedAt);
+        }
+
+    }
+}
